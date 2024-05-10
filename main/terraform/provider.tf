d96be354a4df2e0224932764007e782d8f1a4f81
SergeiShulga <128215486+SergeiShulga@users.noreply.github.com>
2024-05-11 01:23:59 +0400

    Add files via upload

---
+++ main/terraform/provider.tf
@@ -0,0 +1,17 @@
+###  provider 
+
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token     = "*********************************************************"
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = "ru-central1-a"
+}
---
+++ main/terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "cloud_id" {
+  default = "b1g9g7r183chh7fal746"
+}
+variable "folder_id" {
+  default = "b1gelkmoapvj75knumo4"
+}
